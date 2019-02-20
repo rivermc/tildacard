cf3358839405c3bce54e6052f492d61b54dde1b0
Alexey Pakhomov <rivermc@alarstudios.com>
2019-02-20 19:58:57 +0100

    fix ajax msGallery and popupCard initialization

--- assets/template/js/main.js
+++ assets/template/js/main.js
@@ -1,37 +1,58 @@
 $(document).ready(function() {
     
-    function getCard(itemID) {
-        $('.expertise-text').load("/assets/template/php/getCard.php",
-            {
-                id: itemID,
-                snippet: 'GetMyChunk',
-                action: 'getCard',
-                tpl: 'Item'
-            },
-            function(response) {
-                //console.log('getCard response: ' + response);
+    class PopupCard {
+
+        constructor() {
+            this.item = $('.popup-btn');
+        }
+        
+        getPopup() {
+            var self_this = this.item;
+            self_this.magnificPopup({
+                type: 'inline',
+                preloader: false,
+                removalDelay: 350,
+
+                callbacks:  {
+                    beforeOpen: function() {
+                        var itemID = $(this.st.el).data('id');
+
+                        $('.expertise-text').load("/assets/template/php/getCard.php",
+                            {
+                                id: itemID,
+                                snippet: 'GetMyChunk',
+                                action: 'getCard',
+                                tpl: 'Item'
+                            },
+                            function(response) {
+                                //console.log('getCard response: ' + response);
+                            });
+
+                    },
+                    open: function() {
+                        $('html').css('overflow', 'auto');
+                    },
+                    close: function() {
+
+                    }
+                }
             });
+        }
+    }
+
+    var popupcard =  new PopupCard();
+    popupcard.getPopup();
+
+    pdoPage.callbacks['before'] = function() {
+        console.log('Конфиг перед загрузкой!');
+    };
+    pdoPage.callbacks['after'] = function() {
+        console.log('Конфиг после загрузки!');
+        popupcard =  new PopupCard();
+        popupcard.getPopup();
+        miniShop2.Gallery.initialize();
     }
     
-    $('.popup-btn').magnificPopup({
-        type: 'inline',
-        preloader: false,
-        removalDelay: 350,
-    
-        callbacks: {
-            beforeOpen: function() {
-                var itemID = $(this.st.el).data('id');
-                getCard(itemID);
-    
-            },
-            open: function() {
-                $('html').css('overflow', 'auto');
-            },
-            close: function() {
-    
-            }
-        }
-    });
-
+//miniShop2.Gallery.initialize();
 
 });
